3132ef4c4f5d58515e7f4bc2e5d746ca479b01f3
tawfiqjaffar <tawfiq_mtj@hotmail.fr>
2017-08-24 22:55:58 +0200

    add update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-sudo pip3 install pytube
+echo "You are about to install YouTubeDL. This setup script is going to request	for your password to install dependencies."
+sleep 2
+sudo pip3 install --upgrade pytube
+sudo chmod +x run.py
